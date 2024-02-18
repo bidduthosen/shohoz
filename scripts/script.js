8767696d90c3df09faa1558ec5c5f8d3f1aac71c
bidduthosen <mdbidduthosen34@gmail.com>
2024-02-18 22:33:50 +0600

    grand total and conition option done

--- scripts/script.js
+++ scripts/script.js
@@ -7,28 +7,35 @@ let perSeatPrice = 550;
 for(let index = 0; index <allSeatContainer.length ; index++){
     const allSeatList = allSeatContainer[index];
     allSeatList.addEventListener('click', function(){
-        const seat = document.getElementById('seat-count').innerText = count;
-        // total seat
-        totalSeatNumber.innerText = allSeatContainer.length - count;
-        count++;
-
-        const totalPriceContainer = document.getElementById('total-price');
-       totalPriceContainer.innerText = perSeatPrice * seat;
         
-        // per seat number 
-        const perSeatName = allSeatList.innerText;
-
-        const selectSeatContainer = document.getElementById('select-seat-container');
-        const div = document.createElement('div');
-        div.innerHTML = `<div class=" bg-[#F7F8F8] flex justify-between items-center px-5 py-4">
-                            <h3 class="text-base">${perSeatName}</h3>
-                            <h3 class="text-base ml-8">Economoy</h3>
-                            <h3 id="par-seat-price" class="text-base">550</h3>
-                        </div>`;
-      selectSeatContainer.appendChild(div);
-      allSeatList.classList.remove('bg-gray-300')
-      allSeatList.classList.add('bg-[#1DD100]', 'text-white')
-        
+        if(count <= 4){
+            const seat = document.getElementById('seat-count').innerText = count;
+            // total seat
+            totalSeatNumber.innerText = allSeatContainer.length - count;
+            
+            
+            const totalPriceContainer = document.getElementById('total-price');
+            totalPriceContainer.innerText = perSeatPrice * seat;
+            
+            // grand total
+            const grandTotal = document.getElementById('grand-total');
+            grandTotal.innerText = perSeatPrice * seat;
+            
+            // per seat number 
+            const perSeatName = allSeatList.innerText;
+            
+            const selectSeatContainer = document.getElementById('select-seat-container');
+            const div = document.createElement('div');
+            div.innerHTML = `<div class=" bg-[#F7F8F8] flex justify-between items-center px-5 py-4">
+                                  <h3 class="text-base">${perSeatName}</h3>
+                                  <h3 class="text-base ml-8">Economoy</h3>
+                                  <h3 id="par-seat-price" class="text-base">550</h3>
+                              </div>`;
+            selectSeatContainer.appendChild(div);
+            allSeatList.classList.remove('bg-gray-300')
+            allSeatList.classList.add('bg-[#1DD100]', 'text-white');
+        }
+      count++;
     })
 };
 
@@ -37,5 +44,32 @@ for(let index = 0; index <allSeatContainer.length ; index++){
 
 document.getElementById('coupon-apply-btn').addEventListener('click', function(){
     const couponInputField = document.getElementById('coupon-input-field').value;
-    console.log(couponInputField)
+    const couponInputText = couponInputField.split(' ').join('').toUpperCase();
+
+    // grand total
+    const totalPriceContainer = document.getElementById('total-price').innerText;
+    const totalPrice = parseInt(totalPriceContainer);
+    const grandTotal = document.getElementById('grand-total');
+
+    // coupon container
+    const couponContainer = document.getElementById('coupon-container');
+
+
+    if(couponInputText === 'NEW15'){
+       const  discountPrice = totalPrice * 15 / 100;
+       grandTotal.innerText = totalPrice - discountPrice;
+       couponContainer.classList.add('hidden')
+        document.getElementById('coupon-input-field').value = "";
+        
+    }
+    else if(couponInputText === 'COUPLE20'){
+        const  discountPrice = totalPrice * 20 / 100;
+        grandTotal.innerText = totalPrice - discountPrice;
+        couponContainer.classList.add('hidden')
+        document.getElementById('coupon-input-field').value = "";
+    }
+    else{
+        alert('invalid coupon code! plz try again')
+        document.getElementById('coupon-input-field').value = "";
+    }
 })
