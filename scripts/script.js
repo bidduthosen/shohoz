a2d826a7044b753d1246d90ee51ba86202a9af36
bidduthosen <mdbidduthosen34@gmail.com>
2024-02-18 19:13:52 +0600

    total seat and seat count done

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,19 @@
-console.log('connected')
+const allSeatContainer = document.querySelectorAll('h5');
+const totalSeatNumber = document.getElementById('total-seat');
+        totalSeatNumber.innerText = allSeatContainer.length;
+
+let count = 1;
+for(let index = 0; index <allSeatContainer.length ; index++){
+    const allSeatList = allSeatContainer[index];
+    allSeatList.addEventListener('click', function(){
+        const seatCountText = document.getElementById('seat-count').innerText = count;
+        
+        console.log(allSeatList);
+
+        // total seat
+        
+        totalSeatNumber.innerText = allSeatContainer.length - count;
+        console.log( totalSeatNumber)
+        count++;
+    })
+}
