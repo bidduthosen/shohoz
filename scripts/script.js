661623a6dd5996d9ffcea35084ed730b1d7d74e4
bidduthosen <mdbidduthosen34@gmail.com>
2024-02-18 19:51:25 +0600

    per seat price set

--- scripts/script.js
+++ scripts/script.js
@@ -3,17 +3,29 @@ const totalSeatNumber = document.getElementById('total-seat');
         totalSeatNumber.innerText = allSeatContainer.length;
 
 let count = 1;
+let perSeatPrice = 550;
 for(let index = 0; index <allSeatContainer.length ; index++){
     const allSeatList = allSeatContainer[index];
     allSeatList.addEventListener('click', function(){
-        const seatCountText = document.getElementById('seat-count').innerText = count;
-        
-        console.log(allSeatList);
-
+        const seat = document.getElementById('seat-count').innerText = count;
         // total seat
-        
         totalSeatNumber.innerText = allSeatContainer.length - count;
-        console.log( totalSeatNumber)
         count++;
+
+        const totalPriceContainer = document.getElementById('total-price');
+       totalPriceContainer.innerText = perSeatPrice * seat;
+        
+        // per seat number 
+        const perSeatName = allSeatList.innerText;
+
+        const selectSeatContainer = document.getElementById('select-seat-container');
+        const div = document.createElement('div');
+        div.innerHTML = `<div class=" bg-[#F7F8F8] flex justify-between items-center px-5 py-4">
+                            <h3 class="text-base">${perSeatName}</h3>
+                            <h3 class="text-base ml-8">Economoy</h3>
+                            <h3 id="par-seat-price" class="text-base">550</h3>
+                        </div>`;
+      selectSeatContainer.appendChild(div);
+        
     })
 }
